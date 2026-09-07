564c8c7b5e6954f6c2e4211fb2cec225bb354044
yuliaishtar-hub <yuliaishtar@gmail.com>
2026-09-07 19:28:35 +0300

    Add family.js file in lessons directory
    Created a new file 'family.js' in the lessons directory.

---
+++ lessons/family.js
@@ -0,0 +1,13 @@
+english-adventure
+│
+├── index.html          ← пока НЕ трогаем
+│
+├── css
+│   └── style.css       ✅
+│
+├── js
+│   ├── player.js       ✅
+│   └── voice.js        ✅
+│
+└── lessons
+    └── family.js       ← сейчас создаём
